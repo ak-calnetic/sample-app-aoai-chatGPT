a576a5c3e7b0bcf2b8a77d567d7333d7dc8d1903
ANIL KOERI <akoeri@calnetic.com>
2025-06-02 15:03:40 -0400

    filter code updated

--- backend/settings.py
+++ backend/settings.py
@@ -335,6 +335,35 @@ class _AzureSearchSettings(BaseSettings, DatasourcePayloadConstructor):
         
         return None
             
+    # def construct_payload_configuration(
+    #     self,
+    #     *args,
+    #     **kwargs
+    # ):
+    #     request = kwargs.pop('request', None)
+    #     CaseID = kwargs.pop('CaseID', None)
+    #     case_id_filter = ''
+    #     if CaseID is not None and CaseID != "":
+    #         prefix = f"https://truckaccidents001.blob.core.windows.net/truck-accidents-blob/{CaseID}"
+    #         case_id_filter = f"startswith(sfUrl, '{prefix}')"
+    #         # case_id_filter = "search.ismatchscoring('"+ CaseID +"','sfUrl')"
+
+    #     if request and self.permitted_groups_column:
+    #         self.filter = self._set_filter_string(request) + " && " + case_id_filter
+    #     else:
+    #         self.filter = case_id_filter
+            
+    #     self.embedding_dependency = \
+    #         self._settings.azure_openai.extract_embedding_dependency()
+    #     parameters = self.model_dump(exclude_none=True, by_alias=True)
+    #     parameters.update(self._settings.search.model_dump(exclude_none=True, by_alias=True))
+        
+    #     return {
+    #         "type": self._type,
+    #         "parameters": parameters
+    #     }
+
+
     def construct_payload_configuration(
         self,
         *args,
@@ -343,26 +372,25 @@ class _AzureSearchSettings(BaseSettings, DatasourcePayloadConstructor):
         request = kwargs.pop('request', None)
         CaseID = kwargs.pop('CaseID', None)
         case_id_filter = ''
-        if CaseID is not None and CaseID != "":
+    
+        if CaseID:
             prefix = f"https://truckaccidents001.blob.core.windows.net/truck-accidents-blob/{CaseID}"
             case_id_filter = f"startswith(sfUrl, '{prefix}')"
-            # case_id_filter = "search.ismatchscoring('"+ CaseID +"','sfUrl')"
-
+    
         if request and self.permitted_groups_column:
-            self.filter = self._set_filter_string(request) + " && " + case_id_filter
+            base_filter = self._set_filter_string(request)
+            self.filter = f"{base_filter} and {case_id_filter}" if case_id_filter else base_filter
         else:
             self.filter = case_id_filter
-            
-        self.embedding_dependency = \
-            self._settings.azure_openai.extract_embedding_dependency()
+    
+        self.embedding_dependency = self._settings.azure_openai.extract_embedding_dependency()
         parameters = self.model_dump(exclude_none=True, by_alias=True)
         parameters.update(self._settings.search.model_dump(exclude_none=True, by_alias=True))
-        
+    
         return {
             "type": self._type,
             "parameters": parameters
         }
-
 
 class _AzureCosmosDbMongoVcoreSettings(
     BaseSettings,
